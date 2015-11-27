f2e271c9763ae6fe2c0d212683aea2fe879e9ccb
MaartenPlatenburg <maartenplatenburg@gmail.com>
2015-11-27 01:14:23 +0100

    Added API and CSS

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var main = function(){
+   $(".nav nav-tabs").click(function(){
 
+   });
+   $.getJSON( "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=95d38c513c13444b6290e62403391b4e", function( data ) {
+      $('#weather').html(data.weather[0].main);
+      $('#description').html(data.weather[0].description);
+      $('#temp').html(data.main.temp);
+      $('#city').html(data.name);
+   });
 };
 
 $(document).ready(main);
